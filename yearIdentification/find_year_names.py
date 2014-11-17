aee1309b9a5fedd16af2ad88441eac1448b0d6c0
Jamie Campbell <Jamie3.14159@gmail.com>
2014-11-17 12:28:42 -0800

    Cleaned and reorganized

--- yearIdentification/find_year_names.py
+++ yearIdentification/find_year_names.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 import re
-AS = open('Amar-Suen_6/Amar-Suen-6_cdli.txt', 'r')
-SH = open('Shulgi_42/Shulgi_42_cdli.txt', 'r')
+AS = open('corpus/pre-identification/Amar-Suen_6/Amar-Suen-6_cdli.txt', 'r')
+SH = open('corpus/pre-identification/Shulgi_42/Shulgi_42_cdli.txt', 'r')
 years = open('yearsList.txt', 'w')
-diff = open('diffList.txt', 'w')
 asyears = set([])
 shyears = set([])
 
@@ -24,15 +23,5 @@ allyears = set([])
 allyears.update(asyears)
 allyears.update(shyears)
 
-diffyears = set([])
-diffyears.update(asyears)
-diffyears.intersection_update(shyears)
-
-print(len(asyears))
-print(len(shyears))
-print(len(allyears))
-print(len(diffyears))
 ay = list(allyears)
-dy = list(diffyears)
 [years.write(l + '\n') for l in sorted(ay)]
-[diff.write(l + '\n') for l in sorted(dy)]
